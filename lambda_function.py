dfb123f1bea4831454787dd03c2693d59436ed38
ramu455 <jagannadhram@gmail.com>
2025-08-20 00:33:36 +0530

    latest commit

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     print(event)
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from GitHub Webhooks Deployment Done!')
+        'body': json.dumps('Hello from GitHub Webhooks Deployment Done!,from github actions')
     }
